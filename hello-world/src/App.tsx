c428911281cc8f6f43fb23ba153dac7363576082
vinod <vinodk2757@gmail.com>
2024-03-19 15:23:00 +0530

    reactjs hello world

--- hello-world/src/App.tsx
+++ hello-world/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import './App.css';
+import './index.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <div>
+
+    <Navbar />
+
+    {/* The rest of your application */}
+
+  </div>
   );  
 }
 
